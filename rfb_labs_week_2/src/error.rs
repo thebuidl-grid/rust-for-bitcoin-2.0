fe6a79a7baeb5c6ff34baf02e691c5cba4d4fa35
afurious <misraisiguzor@gmail.com>
2026-08-07 10:17:26 +0100

    assignment

--- rfb_labs_week_2/src/error.rs
+++ rfb_labs_week_2/src/error.rs
@@ -20,9 +20,42 @@ pub enum TransactionError {
 }
 
 impl fmt::Display for TransactionError {
-    fn fmt(&self, _formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // TODO(Part 4): return a useful message for every error variant.
-        todo!("implement Display for TransactionError")
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TransactionError::NoInputs => write!(formatter, "transaction has no inputs"),
+            TransactionError::NoOutputs => write!(formatter, "transaction has no outputs"),
+            TransactionError::ZeroValueOutput => write!(
+                formatter,
+                "transaction has a zero-value output that is not OpReturn"
+            ),
+            TransactionError::OutputsExceedInputs {
+                total_inputs,
+                total_outputs,
+            } => {
+                write!(
+                    formatter,
+                    "outputs ({} sats) exceed inputs ({} sats)",
+                    total_outputs, total_inputs
+                )
+            }
+            TransactionError::CoinbaseMixedWithRegularInputs => {
+                write!(formatter, "transaction mixes coinbase and regular inputs")
+            }
+            TransactionError::MultipleCoinbaseInputs => {
+                write!(formatter, "transaction has multiple coinbase inputs")
+            }
+            TransactionError::InvalidTxid => write!(formatter, "transaction has an empty txid"),
+            TransactionError::InsufficientFunds {
+                available,
+                required,
+            } => {
+                write!(
+                    formatter,
+                    "insufficient funds: available {} sats, required {} sats",
+                    available, required
+                )
+            }
+        }
     }
 }
 
