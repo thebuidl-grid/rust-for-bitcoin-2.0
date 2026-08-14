dce0fc61c10d17827840bfb7292a65d18a20a734
rozzy <rosekaremeri@gmail.com>
2026-08-14 08:10:42 +0300

    library error display messages

--- rfb_labs_week_2_session_4/src/error.rs
+++ rfb_labs_week_2_session_4/src/error.rs
@@ -41,10 +41,54 @@ pub enum LibraryError {
 }
 
 impl fmt::Display for LibraryError {
-    fn fmt(&self, _formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // TODO(Part 2): return a useful, human-readable message for every
-        // variant. Include the ids and numbers the variant carries.
-        todo!("implement Display for LibraryError")
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            LibraryError::EmptyTitle => {
+                write!(formatter, "item title cannot be empty")
+            }
+            LibraryError::DuplicateItemId { id } => {
+                write!(formatter, "item with id {} already exists", id)
+            }
+            LibraryError::DuplicateMemberId { id } => {
+                write!(formatter, "member with id {} already exists", id)
+            }
+            LibraryError::ItemNotFound { id } => {
+                write!(formatter, "item with id {} was not found", id)
+            }
+            LibraryError::MemberNotFound { id } => {
+                write!(formatter, "member with id {} was not found", id)
+            }
+            LibraryError::ItemAlreadyOnLoan { id, member_id } => {
+                write!(
+                    formatter,
+                    "item {} is already on loan to member {}",
+                    id, member_id
+                )
+            }
+            LibraryError::ItemNotOnLoan { id } => {
+                write!(formatter, "item {} is not currently on loan", id)
+            }
+            LibraryError::ItemIsLost { id } => {
+                write!(formatter, "item {} is lost", id)
+            }
+            LibraryError::BorrowLimitReached { member_id, limit } => {
+                write!(
+                    formatter,
+                    "member {} has reached the borrow limit of {} items",
+                    member_id, limit
+                )
+            }
+            LibraryError::InvalidReturnDay {
+                day_borrowed,
+                day_returned,
+            } => {
+                write!(
+                    formatter,
+                    "return day {} is before borrow day {}",
+                    day_returned, day_borrowed
+                )
+            }
+        }
     }
 }
 
