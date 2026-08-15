2db961044ee2fb5b8707a5fc866dc72755033882
dorinengabdoh <ngabdohdonfack657@gmail.com>
2026-08-15 15:46:38 +0500

    Complete Week 3: Bitcoin transaction decoder implementation and tests

--- rfb_labs_week_3/decodetrx/src/transaction.rs
+++ rfb_labs_week_3/decodetrx/src/transaction.rs
@@ -1,7 +1,4 @@
-
 use serde::{Serialize, Serializer};
-
-
 
 #[derive(Debug, Serialize)]
 pub struct Transaction {
@@ -12,40 +9,42 @@ pub struct Transaction {
     pub lock_time: u32,
 }
 
+fn as_btc<S: Serializer, T: BitcoinValue>(t: &T, s: S) -> Result<S::Ok, S::Error> {
+    s.serialize_f64(t.to_btc())
+}
+
+fn as_hex<S: Serializer>(bytes: &[u8], s: S) -> Result<S::Ok, S::Error> {
+    s.serialize_str(&hex::encode(bytes))
+}
 
 #[derive(Debug, Serialize)]
 pub struct Input {
-    pub txid: Txid, // [u8; 32],
+    pub txid: Txid,
     pub output_index: u32,
+    #[serde(serialize_with = "as_hex")]
     pub script_sig: Vec<u8>,
-    pub sequence: u32
+    pub sequence: u32,
 }
 
 #[derive(Debug, Serialize)]
 pub struct Output {
     #[serde(serialize_with = "as_btc")]
     pub amount: Amount,
+    #[serde(serialize_with = "as_hex")]
     pub script_pubkey: Vec<u8>,
 }
 
-fn as_btc<S: Serializer, T: BitcoinValue>(t: &T, s: S) -> Result<S::Ok, S::Error> {
-
-}
-
-#[derive(Debug)]
-pub struct Amount( u64);
+#[derive(Debug, Clone, Copy)]
+pub struct Amount(pub u64);
 
 impl Amount {
-  // type associated functiion that initiate the instance of the struct i.e Amount
-  pub fn from_sat(satoshi: u64) -> Amount {
-    Amount(satoshi)
-  }
+    pub fn from_sat(satoshi: u64) -> Amount {
+        Amount(satoshi)
+    }
 }
 
-#[derive(Debug)]
-pub struct Txid([u8; 32]);
-
-// [u8; 32] => array of 32 element each element is 1 byte [u8]; i.e one byte is u8;
+#[derive(Debug, Clone, Copy)]
+pub struct Txid(pub [u8; 32]);
 
 impl Txid {
     pub fn from_bytes(bytes: [u8; 32]) -> Txid {
@@ -55,10 +54,11 @@ impl Txid {
 
 impl Serialize for Txid {
     fn serialize<S: Serializer>(&self, s: S) -> Result<S::Ok, S::Error> {
-       
+        let mut bytes = self.0;
+        bytes.reverse();
+        s.serialize_str(&hex::encode(bytes))
     }
 }
-
 
 trait BitcoinValue {
     fn to_btc(&self) -> f64;
@@ -66,12 +66,6 @@ trait BitcoinValue {
 
 impl BitcoinValue for Amount {
     fn to_btc(&self) -> f64 {
-       
+        self.0 as f64 / 100_000_000.0
     }
 }
-
-
-
-
-
-
