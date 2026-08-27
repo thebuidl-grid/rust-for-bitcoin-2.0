aa059a386a03532fe1a6681db14647ffcbed65a7
rozzy <rosekaremeri@gmail.com>
2026-08-27 17:23:24 +0300

    raw bitcoin data is in bytes, decode to get a Rust struct

--- rfb_labs_week_3/decodetrx/src/transaction.rs
+++ rfb_labs_week_3/decodetrx/src/transaction.rs
@@ -1,7 +1,4 @@
-
 use serde::{Serialize, Serializer};
-
-
 
 #[derive(Debug, Serialize)]
 pub struct Transaction {
@@ -12,13 +9,12 @@ pub struct Transaction {
     pub lock_time: u32,
 }
 
-
 #[derive(Debug, Serialize)]
 pub struct Input {
-    pub txid: Txid, // [u8; 32],
+    pub txid: Txid,
     pub output_index: u32,
     pub script_sig: Vec<u8>,
-    pub sequence: u32
+    pub sequence: u32,
 }
 
 #[derive(Debug, Serialize)]
@@ -28,24 +24,24 @@ pub struct Output {
     pub script_pubkey: Vec<u8>,
 }
 
-fn as_btc<S: Serializer, T: BitcoinValue>(t: &T, s: S) -> Result<S::Ok, S::Error> {
-
+fn as_btc<S: Serializer, T: BitcoinValue>(
+    t: &T,
+    s: S,
+) -> Result<S::Ok, S::Error> {
+    s.serialize_f64(t.to_btc())
 }
 
 #[derive(Debug)]
-pub struct Amount( u64);
+pub struct Amount(u64);
 
 impl Amount {
-  // type associated functiion that initiate the instance of the struct i.e Amount
-  pub fn from_sat(satoshi: u64) -> Amount {
-    Amount(satoshi)
-  }
+    pub fn from_sat(satoshi: u64) -> Amount {
+        Amount(satoshi)
+    }
 }
 
 #[derive(Debug)]
 pub struct Txid([u8; 32]);
-
-// [u8; 32] => array of 32 element each element is 1 byte [u8]; i.e one byte is u8;
 
 impl Txid {
     pub fn from_bytes(bytes: [u8; 32]) -> Txid {
@@ -55,10 +51,15 @@ impl Txid {
 
 impl Serialize for Txid {
     fn serialize<S: Serializer>(&self, s: S) -> Result<S::Ok, S::Error> {
-       
+        let hex = self
+            .0
+            .iter()
+            .map(|byte| format!("{:02x}", byte))
+            .collect::<String>();
+
+        s.serialize_str(&hex)
     }
 }
-
 
 trait BitcoinValue {
     fn to_btc(&self) -> f64;
@@ -66,12 +67,6 @@ trait BitcoinValue {
 
 impl BitcoinValue for Amount {
     fn to_btc(&self) -> f64 {
-       
+        self.0 as f64 / 100_000_000.0
     }
 }
-
-
-
-
-
-
